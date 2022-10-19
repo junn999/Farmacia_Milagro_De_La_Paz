a7379bad3972c31e4aaf4fe7b3ba8229b3f6a040
Hazel Granados <hazelbgranados@gmail.com>
2022-10-19 07:49:15 -0900

    Medicamentos

--- Farmacia_Milagro_De_La_Paz/PL/Medicamentos.Designer.cs
+++ Farmacia_Milagro_De_La_Paz/PL/Medicamentos.Designer.cs
@@ -37,35 +37,37 @@ namespace Farmacia_Milagro_De_La_Paz.PL
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox8 = new System.Windows.Forms.TextBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.textBox9 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txtNdeproducto = new System.Windows.Forms.TextBox();
+            this.txtPresentacion = new System.Windows.Forms.TextBox();
+            this.txtFdecaducidad = new System.Windows.Forms.TextBox();
+            this.txtPdecompra = new System.Windows.Forms.TextBox();
+            this.txtPdeventa = new System.Windows.Forms.TextBox();
+            this.txtganancia = new System.Windows.Forms.TextBox();
+            this.txtInventario = new System.Windows.Forms.TextBox();
+            this.dtgDdemedicamentos = new System.Windows.Forms.DataGridView();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.cbxProveedor = new System.Windows.Forms.ComboBox();
             this.label9 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgDdemedicamentos)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(20, 268);
+            this.label1.Location = new System.Drawing.Point(27, 330);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.Size = new System.Drawing.Size(74, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Proveedor";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(20, 35);
+            this.label2.Location = new System.Drawing.Point(27, 43);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.Size = new System.Drawing.Size(141, 17);
             this.label2.TabIndex = 1;
             this.label2.Text = "Nombre del producto";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -73,18 +75,20 @@ namespace Farmacia_Milagro_De_La_Paz.PL
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(20, 72);
+            this.label3.Location = new System.Drawing.Point(27, 89);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.Size = new System.Drawing.Size(91, 17);
             this.label3.TabIndex = 2;
             this.label3.Text = "Presentacion";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(19, 107);
+            this.label4.Location = new System.Drawing.Point(25, 132);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(105, 13);
+            this.label4.Size = new System.Drawing.Size(136, 17);
             this.label4.TabIndex = 3;
             this.label4.Text = "Fecha de caducidad";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -92,9 +96,10 @@ namespace Farmacia_Milagro_De_La_Paz.PL
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(19, 148);
+            this.label5.Location = new System.Drawing.Point(25, 182);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(90, 13);
+            this.label5.Size = new System.Drawing.Size(119, 17);
             this.label5.TabIndex = 4;
             this.label5.Text = "Precio de compra";
             this.label5.Click += new System.EventHandler(this.label5_Click);
@@ -102,128 +107,143 @@ namespace Farmacia_Milagro_De_La_Paz.PL
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(20, 184);
+            this.label6.Location = new System.Drawing.Point(27, 226);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(82, 13);
+            this.label6.Size = new System.Drawing.Size(107, 17);
             this.label6.TabIndex = 5;
             this.label6.Text = "Precio de venta";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(20, 228);
+            this.label7.Location = new System.Drawing.Point(27, 281);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(53, 13);
+            this.label7.Size = new System.Drawing.Size(69, 17);
             this.label7.TabIndex = 6;
             this.label7.Text = "Ganancia";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(464, 72);
+            this.label8.Location = new System.Drawing.Point(619, 89);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(54, 13);
+            this.label8.Size = new System.Drawing.Size(70, 17);
             this.label8.TabIndex = 7;
             this.label8.Text = "Inventario";
             // 
-            // textBox2
+            // txtNdeproducto
             // 
-            this.textBox2.Location = new System.Drawing.Point(130, 32);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(312, 20);
-            this.textBox2.TabIndex = 9;
+            this.txtNdeproducto.Location = new System.Drawing.Point(173, 39);
+            this.txtNdeproducto.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNdeproducto.Name = "txtNdeproducto";
+            this.txtNdeproducto.Size = new System.Drawing.Size(415, 22);
+            this.txtNdeproducto.TabIndex = 9;
             // 
-            // textBox3
+            // txtPresentacion
             // 
-            this.textBox3.Location = new System.Drawing.Point(130, 69);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(312, 20);
-            this.textBox3.TabIndex = 10;
+            this.txtPresentacion.Location = new System.Drawing.Point(173, 85);
+            this.txtPresentacion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPresentacion.Name = "txtPresentacion";
+            this.txtPresentacion.Size = new System.Drawing.Size(415, 22);
+            this.txtPresentacion.TabIndex = 10;
             // 
-            // textBox4
+            // txtFdecaducidad
             // 
-            this.textBox4.Location = new System.Drawing.Point(130, 104);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(312, 20);
-            this.textBox4.TabIndex = 11;
+            this.txtFdecaducidad.Location = new System.Drawing.Point(173, 128);
+            this.txtFdecaducidad.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtFdecaducidad.Name = "txtFdecaducidad";
+            this.txtFdecaducidad.Size = new System.Drawing.Size(415, 22);
+            this.txtFdecaducidad.TabIndex = 11;
             // 
-            // textBox5
+            // txtPdecompra
             // 
-            this.textBox5.Location = new System.Drawing.Point(130, 145);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(312, 20);
-            this.textBox5.TabIndex = 12;
+            this.txtPdecompra.Location = new System.Drawing.Point(173, 178);
+            this.txtPdecompra.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPdecompra.Name = "txtPdecompra";
+            this.txtPdecompra.Size = new System.Drawing.Size(415, 22);
+            this.txtPdecompra.TabIndex = 12;
             // 
-            // textBox6
+            // txtPdeventa
             // 
-            this.textBox6.Location = new System.Drawing.Point(130, 181);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(312, 20);
-            this.textBox6.TabIndex = 13;
+            this.txtPdeventa.Location = new System.Drawing.Point(173, 223);
+            this.txtPdeventa.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPdeventa.Name = "txtPdeventa";
+            this.txtPdeventa.Size = new System.Drawing.Size(415, 22);
+            this.txtPdeventa.TabIndex = 13;
             // 
-            // textBox7
+            // txtganancia
             // 
-            this.textBox7.Location = new System.Drawing.Point(130, 225);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(312, 20);
-            this.textBox7.TabIndex = 14;
+            this.txtganancia.Location = new System.Drawing.Point(173, 277);
+            this.txtganancia.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtganancia.Name = "txtganancia";
+            this.txtganancia.Size = new System.Drawing.Size(415, 22);
+            this.txtganancia.TabIndex = 14;
             // 
-            // textBox8
+            // txtInventario
             // 
-            this.textBox8.Location = new System.Drawing.Point(540, 69);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(109, 20);
-            this.textBox8.TabIndex = 15;
+            this.txtInventario.Location = new System.Drawing.Point(720, 85);
+            this.txtInventario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtInventario.Name = "txtInventario";
+            this.txtInventario.Size = new System.Drawing.Size(144, 22);
+            this.txtInventario.TabIndex = 15;
             // 
-            // dataGridView1
+            // dtgDdemedicamentos
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(23, 292);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(626, 187);
-            this.dataGridView1.TabIndex = 16;
+            this.dtgDdemedicamentos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgDdemedicamentos.Location = new System.Drawing.Point(31, 359);
+            this.dtgDdemedicamentos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dtgDdemedicamentos.Name = "dtgDdemedicamentos";
+            this.dtgDdemedicamentos.RowHeadersWidth = 51;
+            this.dtgDdemedicamentos.Size = new System.Drawing.Size(835, 230);
+            this.dtgDdemedicamentos.TabIndex = 16;
             // 
-            // textBox9
+            // txtID
             // 
-            this.textBox9.Location = new System.Drawing.Point(540, 31);
-            this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(109, 20);
-            this.textBox9.TabIndex = 17;
+            this.txtID.Location = new System.Drawing.Point(720, 38);
+            this.txtID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(144, 22);
+            this.txtID.TabIndex = 17;
             // 
-            // comboBox1
+            // cbxProveedor
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(130, 265);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(312, 21);
-            this.comboBox1.TabIndex = 18;
+            this.cbxProveedor.FormattingEnabled = true;
+            this.cbxProveedor.Location = new System.Drawing.Point(173, 326);
+            this.cbxProveedor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbxProveedor.Name = "cbxProveedor";
+            this.cbxProveedor.Size = new System.Drawing.Size(415, 24);
+            this.cbxProveedor.TabIndex = 18;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(464, 34);
+            this.label9.Location = new System.Drawing.Point(619, 42);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(18, 13);
+            this.label9.Size = new System.Drawing.Size(21, 17);
             this.label9.TabIndex = 19;
             this.label9.Text = "ID";
             this.label9.Click += new System.EventHandler(this.label9_Click);
             // 
             // Medicamentos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(674, 504);
+            this.ClientSize = new System.Drawing.Size(899, 620);
             this.Controls.Add(this.label9);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.textBox9);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.textBox8);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.cbxProveedor);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.dtgDdemedicamentos);
+            this.Controls.Add(this.txtInventario);
+            this.Controls.Add(this.txtganancia);
+            this.Controls.Add(this.txtPdeventa);
+            this.Controls.Add(this.txtPdecompra);
+            this.Controls.Add(this.txtFdecaducidad);
+            this.Controls.Add(this.txtPresentacion);
+            this.Controls.Add(this.txtNdeproducto);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -232,9 +252,10 @@ namespace Farmacia_Milagro_De_La_Paz.PL
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Medicamentos";
             this.Text = "Medicamentos";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgDdemedicamentos)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -250,16 +271,16 @@ namespace Farmacia_Milagro_De_La_Paz.PL
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.TextBox textBox8;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.TextBox textBox9;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txtNdeproducto;
+        private System.Windows.Forms.TextBox txtPresentacion;
+        private System.Windows.Forms.TextBox txtFdecaducidad;
+        private System.Windows.Forms.TextBox txtPdecompra;
+        private System.Windows.Forms.TextBox txtPdeventa;
+        private System.Windows.Forms.TextBox txtganancia;
+        private System.Windows.Forms.TextBox txtInventario;
+        private System.Windows.Forms.DataGridView dtgDdemedicamentos;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.ComboBox cbxProveedor;
         private System.Windows.Forms.Label label9;
     }
 }
