9c29bb1f467d915c8b481a82d33aa1818ea8fa73
JUNIOR <junioralberto1421@gmail.com>
2022-10-20 18:56:58 -0600

    1

--- Farmacia_Milagro_De_La_Paz/BLL/ProveedorBLL.cs
+++ Farmacia_Milagro_De_La_Paz/BLL/ProveedorBLL.cs
@@ -8,6 +8,25 @@ namespace Farmacia_Milagro_De_La_Paz.BLL
 {
     public class ProveedorBLL
     {
+        private int id;
+        private string laboratorios;
+        private string nombre_del_encargado;
+        private string telefono;
+        private string direccion;
 
+        public ProveedorBLL(int id, string laboratorios, string nombre_del_encargado, string telefono, string direccion)
+        {
+            this.id = id;
+            this.laboratorios = laboratorios;
+            this.nombre_del_encargado = nombre_del_encargado;
+            this.telefono = telefono;
+            this.direccion = direccion;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Laboratorios { get => laboratorios; set => laboratorios = value; }
+        public string Nombre_del_encargado { get => nombre_del_encargado; set => nombre_del_encargado = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
     }
 }
--- Farmacia_Milagro_De_La_Paz/BLL/VentasBLL.cs
+++ Farmacia_Milagro_De_La_Paz/BLL/VentasBLL.cs
@@ -9,27 +9,32 @@ namespace Farmacia_Milagro_De_La_Paz.BLL
     public class VentasBLL
     {
         private int id;
-        private string ID_Empleados;
-        private string ID_Medicamentos;
-        private string Id_Papeleria;
-        private string Cantidad_Productos;
-        private string Total;
+        private string id_empleados;
+        private string id_medicamentos;
+        private string id_papeleria;
+        private string cantidad_productos;
+        private string total;
 
-        public VentasBLL(int id, string Id_Empleados, string Id_Medicamentos, string Id_Papeleria, string Cantidad_Productos, string Total)
+        public VentasBLL(int id)
         {
             this.id = id;
-            this.Id_Empleados = Id_Empleados;
-            this.Id_Medicamentos = Id_Medicamentos;
-            this.Id_Papeleria = Id_Papeleria;
-            this.Cantidad_Productos = Cantidad_Productos;
-            this.Total = Total;
+        }
+
+        public VentasBLL(int id, string Id_Empleados, string Id_Medicamentos, string Id_Papeleria, string Cantidad_Productos, string Total, string id_Empleados, string id_Medicamentos, string id_Papeleria, string cantidad_Productos, string total)
+        {
+            this.id = id;
+            this.id_empleados = id_Empleados;
+            this.id_medicamentos = id_Medicamentos;
+            this.id_papeleria = id_Papeleria;
+            this.cantidad_productos = cantidad_Productos;
+            this.total = total;
         }
 
         public int Id { get => id; set => id = value; }
-        public string Id_Empleados { get => Id_Empleados; set => Id_Empleados = value; }
-        public string Id_Medicamentos { get => Id_Medicamentos; set => Id_Medicamentos = value; }
-        public string Id_Papeleria1 { get => Id_Papeleria; set => Id_Papeleria = value; }
-        public string Cantidad_Productos { get => Cantidad_Productos; set => Cantidad_Productos = value; }
-        public string Total { get => Total; set => Total = value; }
+        public string Id_Empleados { get => id_empleados; set => id_empleados = value; }
+        public string Id_Medicamentos { get => id_medicamentos; set => id_medicamentos = value; }
+        public string Id_Papeleria { get => id_papeleria; set => id_papeleria = value; }
+        public string Cantidad_Productos { get => cantidad_productos; set => cantidad_productos = value; }
+        public string Total { get => total; set => total = value; }
     }
 }
